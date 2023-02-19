ab6addd7eab9ea08debbe2f87e009ee2750e565f
seol <seol11796@gmail.com>
2023-02-20 01:20:37 +0900

    JPA Auditing model added

---
+++ dugeun/src/main/java/com/project/dugeun/model/domain/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package com.project.dugeun.model.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
---
+++ dugeun/src/main/java/com/project/dugeun/model/domain/blindDate/Participant.java
@@ -0,0 +1,16 @@
+package com.project.dugeun.model.domain.blindDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+public class Participant {
+}
