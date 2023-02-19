906e845c5bc5ba0ed1715b854d7d218cdd83e7c9
seol <seol11796@gmail.com>
2023-02-20 01:31:57 +0900

    participant model added

--- dugeun/src/main/java/com/project/dugeun/model/domain/blindDate/Participant.java
+++ dugeun/src/main/java/com/project/dugeun/model/domain/blindDate/Participant.java
@@ -1,16 +1,31 @@
 package com.project.dugeun.model.domain.blindDate;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.project.dugeun.model.domain.BaseTimeEntity;
+import com.project.dugeun.model.domain.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Entity
-public class Participant {
+public class Participant extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JsonIgnore
+    private User user;
+
+    private LocalDate nextMatchingDate;
+
+
 }
