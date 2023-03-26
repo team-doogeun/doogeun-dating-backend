ee4fcea5b379d7541137274260199cc2f8999e70
seol11796 <seol11796@gmail.com>
2023-03-26 17:42:23 +0900

    matching logic (consider first,second,third priority)

--- dugeun/src/main/java/com/project/dugeun/domain/blindDate/application/MatchMaker.java
+++ dugeun/src/main/java/com/project/dugeun/domain/blindDate/application/MatchMaker.java
@@ -28,19 +28,19 @@ public class MatchMaker {
 
 
         // 주어진 유저를 제외한 모두 유저들 불러오기
-      List<User> users = userRepository.findAllByUserIdNot(user.getUserId());
-      List<User> matches = new ArrayList<>();
+        List<User> users = userRepository.findAllByUserIdNot(user.getUserId());
+        List<User> matches = new ArrayList<>();
 
-      for(User otherUser: users){
-          int compatibilityScore = calculateCompatibility(user,otherUser);
-          System.out.println(compatibilityScore);
-          // 점수가 60점 이상이면
-          if(compatibilityScore >= 20){
-            matches.add(otherUser);
-            saveMatch(user,otherUser, compatibilityScore);
-          }
-      }
-      return matches;
+        for(User otherUser: users){
+            int compatibilityScore = calculateCompatibility(user,otherUser);
+            System.out.println(compatibilityScore);
+            // 점수가 60점 이상이면
+            if(compatibilityScore >= 20){
+                matches.add(otherUser);
+                saveMatch(user,otherUser, compatibilityScore);
+            }
+        }
+        return matches;
     }
 
 
@@ -57,56 +57,139 @@ public class MatchMaker {
         ArrayList<String> idealHobbies = new ArrayList<>();
         ArrayList<String> targetHobbies = new ArrayList<>();
 
-        if(firstPri == "나이"){
+        // ** 점수 내는 로직 **
 
+
+        // 나이
+        String idealAgeType = user1.getIdealTypeProfile().getIdealAge().getValue();
+        Integer targetedAge = user2.getAge();
+        String targetedAgeType = "";
+
+        if(targetedAge <= 23){
+            targetedAgeType = "20대 초반";
+        }
+        else if(targetedAge>=24 && targetedAge <27){
+            targetedAgeType = "20대 중반";
+        }
+        else if(targetedAge>= 27 && targetedAge<30){
+            targetedAgeType = "20대 후반";
+        }
+        else if(targetedAge >=30){
+            targetedAgeType = "30대 초반";
         }
 
-        // 이상형의 학과가 같은 결우 점수 부여
-        else if(firstPri == "학과"){
+        if(targetedAgeType == idealAgeType){
+            score += 10;
 
-            if(user1.getIdealTypeProfile().getIdealDepartment().getValue() == user2.getDetailProfile().getDepartment().getValue()){
+            if(user1.getDetailProfile().getFirstPriority().getValue() == "나이"){
                 score += 20;
             }
-        }
-
-        // 주소지가 가까우면 점수 부여
-        else if(firstPri == "주소"){
-
-            if(user1.getDetailProfile().getAddress().getValue() == user2.getDetailProfile().getAddress().getValue()){
-                score+=20;
+            else if(user1.getDetailProfile().getSecondPriority().getValue() == "나이"){
+                score += 10;
+            }
+            else if(user1.getDetailProfile().getThirdPriority().getValue() == "나이"){
+                score += 5;
             }
 
 
+        }
+
+        // 학과
+        if(user1.getIdealTypeProfile().getIdealDepartment().getValue() == user2.getDetailProfile().getDepartment().getValue()){
+            score += 10;
+
+            // 우선순위에 해당 카테고리가 있으면 추가 점수
+            if(user1.getDetailProfile().getFirstPriority().getValue() == "학과"){
+                score += 20;
+            }
+
+            else if(user1.getDetailProfile().getSecondPriority().getValue() == "학과"){
+                score += 10;
+            }
+
+            else if(user1.getDetailProfile().getThirdPriority().getValue() == "학과"){
+                score += 5;
+            }
+        }
+
+
+        // 주소 ( 주소지가 가까우면 점수 부여 )
+        if(user1.getDetailProfile().getAddress().getValue() == user2.getDetailProfile().getAddress().getValue()){
+            score+=10;
+            // 우선순위에 해당 카테고리가 있으면 추가 점수
+            if(user1.getDetailProfile().getFirstPriority().getValue() == "주소"){
+                score += 20;
+            }
+
+            else if(user1.getDetailProfile().getSecondPriority().getValue() == "주소"){
+                score += 10;
+            }
+
+            else if(user1.getDetailProfile().getThirdPriority().getValue() == "주소"){
+                score += 5;
+            }
 
         }
-        else if(firstPri == "취미"){
-           targetHobbies.add(user2.getDetailProfile().getHobby1().getValue());
-           targetHobbies.add(user2.getDetailProfile().getHobby2().getValue());
 
-           idealHobbies.add(user1.getIdealTypeProfile().getIdealHobby1().getValue());
-           idealHobbies.add(user1.getIdealTypeProfile().getIdealHobby2().getValue());
 
-           // 15점씩
+
+            // 취미
+            targetHobbies.add(user2.getDetailProfile().getHobby1().getValue());
+            targetHobbies.add(user2.getDetailProfile().getHobby2().getValue());
+
+            idealHobbies.add(user1.getIdealTypeProfile().getIdealHobby1().getValue());
+            idealHobbies.add(user1.getIdealTypeProfile().getIdealHobby2().getValue());
+
+            // 15점씩
             for(int i=0; i<2; i++){
-               if(targetHobbies.contains(idealHobbies.get(i))){
-                   score += 15;
-               }
+                if(targetHobbies.contains(idealHobbies.get(i))){
+
+                    score += 15;
+                    if(user1.getDetailProfile().getFirstPriority().getValue() == "취미"){
+                        score += 20;
+
+                    }else if(user1.getDetailProfile().getSecondPriority().getValue() == "취미"){
+                        score += 10;
+                    }
+                    else if(user1.getDetailProfile().getThirdPriority().getValue() == "취미"){
+                        score += 5;
+                    }
+                }
 
             }
 
-        }
-        else if(firstPri == "성격"){
+
+
+        // 성격
             if(user1.getIdealTypeProfile().getIdealCharacter1().getValue() == user2.getDetailProfile().getCharacter1().getValue()){
-                score+=15;
+                score+=10;
+                if(user1.getDetailProfile().getFirstPriority().getValue() == "성격"){
+                    score += 20;
+
+                }else if(user1.getDetailProfile().getSecondPriority().getValue() == "성격"){
+                    score += 10;
+                }
+                else if(user1.getDetailProfile().getThirdPriority().getValue() == "성격"){
+                    score += 5;
+                }
+
             }
+
             if(user1.getIdealTypeProfile().getIdealCharacter2().getValue()== user2.getDetailProfile().getCharacter2().getValue()){
-                score+=15;
+                score+=10;
+                if(user1.getDetailProfile().getFirstPriority().getValue() == "성격"){
+                    score += 20;
+
+                }else if(user1.getDetailProfile().getSecondPriority().getValue() == "성격"){
+                    score += 10;
+                }
+                else if(user1.getDetailProfile().getThirdPriority().getValue() == "성격"){
+                    score += 5;
+                }
             }
 
-        }
 
-        else if(firstPri == "키"){
-
+        // 키
             String idealHeightTYpe = user1.getIdealTypeProfile().getIdealHeight().getValue();
             Integer targetedHeight = user2.getDetailProfile().getHeight();
             String targetHeightType ="";
@@ -143,43 +226,85 @@ public class MatchMaker {
             }
 
             if(targetHeightType == idealHeightTYpe){
-                score+=20;
+                score+=10;
+                if(user1.getDetailProfile().getFirstPriority().getValue() == "키"){
+                    score += 20;
+
+                }else if(user1.getDetailProfile().getSecondPriority().getValue() == "키"){
+                    score += 10;
+                }
+                else if(user1.getDetailProfile().getThirdPriority().getValue() == "키"){
+                    score += 5;
+                }
             }
 
-        }
 
-        else if(firstPri == "체형"){
-                if(user1.getIdealTypeProfile().getIdealBodyType().getValue() == user2.getDetailProfile().getBodyType().getValue())
-                {
+
+            // 체형
+            if(user1.getIdealTypeProfile().getIdealBodyType().getValue() == user2.getDetailProfile().getBodyType().getValue())
+            {
+                score += 10;
+                if(user1.getDetailProfile().getFirstPriority().getValue() == "체형"){
                     score += 20;
+
+                }else if(user1.getDetailProfile().getSecondPriority().getValue() == "체형"){
+                    score += 10;
+                }
+                else if(user1.getDetailProfile().getThirdPriority().getValue() == "체형"){
+                    score += 5;
+                }
+            }
+
+
+            // 흡연 정도
+            if(user1.getIdealTypeProfile().getIdealSmoke().getValue() == user2.getDetailProfile().getSmoke().getValue())
+            {
+                score += 10;
+                if(user1.getDetailProfile().getFirstPriority().getValue() == "흡연 정도"){
+                    score += 20;
+
+                }else if(user1.getDetailProfile().getSecondPriority().getValue() == "흡연 정도"){
+                    score += 10;
+                }
+                else if(user1.getDetailProfile().getThirdPriority().getValue() == "흡연 정도"){
+                    score += 5;
                 }
 
-        }
-        else if(firstPri == "흡연 정도"){
-            if(user1.getIdealTypeProfile().getIdealSmoke().getValue() == user2.getDetailProfile().getSmoke().getValue())
-            {
-            score+=20;
-
-        }
-        else if(firstPri == "음주 정도"){
-
-            if(user1.getIdealTypeProfile().getIdealSmoke().getValue() == user2.getDetailProfile().getSmoke().getValue())
-            {
-                score += 20;
-            }
             }
 
-        }
-        else if(firstPri == "MBTI"){
+            // 음주 정도
+        if(user1.getIdealTypeProfile().getIdealSmoke().getValue() == user2.getDetailProfile().getSmoke().getValue())
+                {
+                    score += 10;
+                    if(user1.getDetailProfile().getFirstPriority().getValue() == "음주 정도"){
+                        score += 20;
+
+                    }else if(user1.getDetailProfile().getSecondPriority().getValue() == "음주 정도"){
+                        score += 10;
+                    }
+                    else if(user1.getDetailProfile().getThirdPriority().getValue() == "음주 정도"){
+                        score += 5;
+                    }
+                }
+
+
+
+
             if(user1.getIdealTypeProfile().getIdealMbti().getValue() == user2.getDetailProfile().getMbti().getValue()){
-                score += 20;
+                score += 10;
+                if(user1.getDetailProfile().getFirstPriority().getValue() == "MBTI"){
+                    score += 20;
+
+                }else if(user1.getDetailProfile().getSecondPriority().getValue() == "MBTI"){
+                    score += 10;
+                }
+                else if(user1.getDetailProfile().getThirdPriority().getValue() == "MBTI"){
+                    score += 5;
+                }
             }
-
-        }
-
 
         return score;
-//        return 70;
+
     }
 
     // save a match between two users
