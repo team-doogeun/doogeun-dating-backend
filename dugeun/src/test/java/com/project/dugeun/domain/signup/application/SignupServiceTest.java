d146041fe0b82b84c10cea7f4eca0539ab386b53
seol11796 <seol11796@gmail.com>
2023-08-16 15:03:00 +0900

    -

---
+++ dugeun/src/test/java/com/project/dugeun/domain/signup/application/SignupServiceTest.java
@@ -0,0 +1,18 @@
+package com.project.dugeun.domain.signup.application;
+
+import com.project.dugeun.domain.user.domain.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SignupServiceTest {
+
+    @Autowired
+    SignupService signupService;
+
+
+
+}
