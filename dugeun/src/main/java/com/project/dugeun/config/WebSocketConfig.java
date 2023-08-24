2788f144c99457f6f58c86724d3c272a5b673c26
seol11796 <seol11796@gmail.com>
2023-08-24 17:09:48 +0900

    socket Configuration

---
+++ dugeun/src/main/java/com/project/dugeun/config/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package com.project.dugeun.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")   //SockJS 연결 주소
+                .setAllowedOriginPatterns("*")
+                .setAllowedOrigins("http://localhost:8080")
+                .setAllowedOrigins("https://www.doogeun.kr")// spring stomp CORS 설정하기
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+}
